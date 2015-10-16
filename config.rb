e54d47b1504e1f55cf5a12f59f3dad6b0094f439
Leo <leoobjktiv@gmail.com>
2015-10-15 21:28:14 -0400

    career cruising test

---
+++ config.rb
@@ -0,0 +1,8 @@
+preferred_syntax = :sass
+http_path = '/'
+css_dir = 'assets/stylesheet'
+sass_dir = 'assets/sass'
+images_dir = 'assets/images'
+relative_assets = true
+line_comments = true
+output_style = :compressed
